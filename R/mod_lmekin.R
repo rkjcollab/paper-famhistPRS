d5997e73b034bdeb4b90f61cdf9b5199defd1fa2
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2026-08-07 18:27:08 -0600

    Add Wald test

--- R/mod_lmekin.R
+++ R/mod_lmekin.R
@@ -1,15 +1,12 @@
 library(coxme)
 
-# study <- "daisy"
-# pheno <- specs[[study]]$pheno
-# kinship <- specs[[study]]$kinship
-# covs <- specs[[study]]$covs
-# fdr_var <- specs[[study]]$fdr_var
-# fdr_ref <- specs[[study]]$fdr_ref
+# fdr_var <- config$fdr_var
+# fdr_ref <- config$fdr_ref
 
 # Note: pheno files must be set to have id = "ID"
 #' @export
-mod <- function(study, pheno, kinship, outcome, covs, fdr_var, fdr_ref) {
+mod <- function(
+    study, pheno, kinship, outcome, covs, fdr_var, fdr_ref, wald_test = NULL) {
   
   # Read in pheno
   pheno <- readRDS(pheno)
@@ -28,6 +25,36 @@ mod <- function(study, pheno, kinship, outcome, covs, fdr_var, fdr_ref) {
   
   # Run linear mixed model
   mod = lmekin(form, data = pheno, varlist = kinship_mat)
+  
+  # Wald test
+  if (!is.null(wald_test)) {
+    beta <- fixef(mod)
+    v <- vcov(mod)
+    
+    if (grepl(":", wald_test)) {
+      stop("Cannot compute a Wald test on an interaction term.")
+    } else {
+      # Term
+      term_pattern <- paste0("^", wald_test)
+    }
+    term_idx <- which(grepl(term_pattern, names(beta)))
+    if (length(term_idx) == 0) {
+      stop("Wald test term not found in coefficients: ", wald_test)
+    }
+    
+    beta_sub <- beta[term_idx]
+    v_sub <- v[term_idx, term_idx, drop = FALSE]
+    
+    # Wald statistic
+    chisq <- as.numeric(t(beta_sub) %*% solve(v_sub) %*% beta_sub)
+    df <- length(beta_sub)
+    pval <- pchisq(chisq, df = df, lower.tail = FALSE)
+    
+    wald <- data.frame(
+      chisq = chisq,
+      df = df,
+      p = pval)
+  }
   
   # Get overall model values
   results <- data.frame(study = study)
@@ -65,13 +92,28 @@ mod <- function(study, pheno, kinship, outcome, covs, fdr_var, fdr_ref) {
   results_ci_high <- as.data.frame(t(ci_high))
   colnames(results_ci_high) <- paste0("conf.high_", names(mod$coefficients$fixed))
   
+  # Extract Wald test on given variable
+  if (!is.null(wald_test)) {
+    results_w <- data.frame(
+      wald$p,
+      wald$chisq,
+      wald$df)
+    colnames(results_w) <- c(
+      paste0(wald_test, "_global_p"),
+      paste0(wald_test, "_global_chisq"),
+      paste0(wald_test, "_global_df"))
+  } else {
+    results_w <- data.frame()
+  }
+  
   # Combine, standardize names
   results_all_tmp <- cbind(
     results_est, results_se, results_z, results_p,
-    results_ci_low, results_ci_high)
+    results_ci_low, results_ci_high, results_w)
   
   colnames(results_all_tmp) <- colnames(results_all_tmp) %>%
     gsub(fdr_var, "fdr_", .) %>%
+    gsub("__+", "_", .) %>%
     gsub("Mom", "mom", .) %>%
     gsub("None", "none", .) %>%
     gsub("Sib", "sib", .) %>%
